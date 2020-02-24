250fb80963b6d1df68eccde12b983b741d7f823c
Revati6969 <61040382+Revati6969@users.noreply.github.com>
2020-02-24 15:34:40 +0530

    MODIFIED:Removed spaces

--- whileloop/Gambler.sh
+++ whileloop/Gambler.sh
@@ -6,19 +6,19 @@ win=0
 lose=0
 while [[ $money -gt 0 && $money -lt $goal ]]
 do
-	if [[ $((RANDOM%10)) -gt 5 ]]
+   if [[ $((RANDOM%10)) -gt 5 ]]
+   then
+	money=$(($money+1))
+	echo $money money
+	win=$(($win+1))
+	if [[ $money -eq $goal ]]
 	then
-		money=$(($money+1))
-		echo $money money
-		win=$(($win+1))
-		if [[ $money -eq $goal ]]
-		then
-			break
-		fi
-		else
-			lose=$(($lose+1))
-			money=$(($money+1))
-		fi
+	     break
+	fi
+	else
+	     lose=$(($lose+1))
+	     money=$(($money+1))
+	fi
 done
 echo $win win
 echo $lose lose
