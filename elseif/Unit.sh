b30f889482cf3789d7a6d48bc83ef835bd57176b
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-24 11:23:21 +0530

    ADDED:Print unit

---
+++ elseif/Unit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+read -p "enter number" number
+if [[ $number -ge 10000 ]]
+then
+	echo "number must be less or equal to 10000"
+elif [[ $number -eq 1 ]]
+then 
+	echo "Unit"
+elif [[ $number -eq 10 ]]
+then
+	echo "Ten"
+elif [[ $number -eq 100 ]]
+then
+	echo "Hundred"
+elif [[ $number -eq 1000 ]]
+then
+	echo "Thousand"
+else
+	echo "Ten Thousand"
+fi
