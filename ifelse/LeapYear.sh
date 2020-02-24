b014b6b19f4ebe070b442229056ea3edbeda4123
Revati6969 <61040382+Revati6969@users.noreply.github.com>
2020-02-24 14:41:26 +0530

    MODIFIED:Removed spaces

--- ifelse/LeapYear.sh
+++ ifelse/LeapYear.sh
@@ -2,7 +2,7 @@
 read -p "enter year:" year
 if [[ year%4 -eq 0 && year%100 -ne 0 || year%400 -eq 0 ]]
 then
-	echo "year is leap"
+    echo "year is leap"
 else
-	echo "year is not leap"
+    echo "year is not leap"
 fi
