94cca88141dc656a6e0d193c87b687344d92b2ce
Revati6969 <61040382+Revati6969@users.noreply.github.com>
2020-02-24 15:19:24 +0530

    ADDED:Number is prime or not

--- forloop/PrimeOrNot.sh
+++ forloop/PrimeOrNot.sh
@@ -2,17 +2,17 @@
 read -p "enter number : " number
 for (( i=2; i<=$number/2; i++ ))
 do 
-	if [[ $number%2 -eq 0 ]]
-	then
-		flag=0
-	else
-		flag=1
-	fi
+   if [[ $number%2 -eq 0 ]]
+   then
+	flag=0
+   else
+	flag=1
+   fi
 done
 if [[ flag -eq 0 ]]
 then
-	echo $number " is prime"
+     echo $number " is prime"
 else
-	echo $num " is not prime"
+     echo $num " is not prime"
 fi
 
