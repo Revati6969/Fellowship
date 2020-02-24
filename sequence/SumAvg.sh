e1443ac4346f849fc85ce2d6c1c11308120bd42e
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-24 12:41:43 +0530

    FIXED:Correct variable names

--- sequence/SumAvg.sh
+++ sequence/SumAvg.sh
@@ -4,7 +4,7 @@ number2=$(($RANDOM %(89+1-1)+10))
 number3=$(($RANDOM %(89+1-1)+10))
 number4=$(($RANDOM %(89+1-1)+10))
 number5=$(($RANDOM %(89+1-1)+10))
-sum=$(($a+$b+$c+$d+$e))
+sum=$(($number1+$number2+$number3+$number4+$number5))
 avg=$(($sum/5))
 echo $sum
 echo $avg
