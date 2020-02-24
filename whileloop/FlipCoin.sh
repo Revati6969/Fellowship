18c611317408a3da293ad963a4cf9bcdeb2ac6f5
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-24 12:01:57 +0530

    ADDED:Flip coin till head or tail is 11

---
+++ whileloop/FlipCoin.sh
@@ -0,0 +1,20 @@
+#/bin/bash -x
+#Constants
+head=0
+tail=0
+temp=0
+while [[ $temp -eq 0 ]]
+	do
+		if [[ $((RANDOM%10)) -gt 5 ]]
+		then
+			head=$(($head+1))
+		else
+			tail=$(($tail+1))
+		fi
+		if [[ $head -eq 11 || $tail -eq 11 ]]
+		then
+			temp=$(($temp+1))
+		fi
+	done
+echo head $head
+echo tail $tail 
