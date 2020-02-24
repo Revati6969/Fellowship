2ac471683a83112be3f05ea3d2df7748fee6b741
Revati6969 <61040382+Revati6969@users.noreply.github.com>
2020-02-24 14:33:55 +0530

    MODIFIED:Removed spaces

--- elseif/NumberToWord.sh
+++ elseif/NumberToWord.sh
@@ -5,34 +5,34 @@ then
 	echo "number must be greater than 0"
 elif [[ $number -ge 10 ]]
 then 
-	echo "number must be less than 10"
+    echo "number must be less than 10"
 elif [[ $number -eq 0 ]]
 then 
-	echo "ZERO"
+    echo "ZERO"
 elif [[ $number -eq 1 ]]
 then
-	echo "ONE"
+    echo "ONE"
 elif [[ $number -eq 2 ]]
 then
-	echo "TWO"
+    echo "TWO"
 elif [[ $number -eq 3 ]]
 then 
-	echo "THREE"
+    echo "THREE"
 elif [[ $number -eq 4 ]]
 then 
-	echo "FOUR"
+    echo "FOUR"
 elif [[ $number -eq 5 ]]
 then 
-   echo "FIVE"
+    echo "FIVE"
 elif [[ $number -eq 6 ]]
 then 
-   echo "SIX"
+    echo "SIX"
 elif [[ $number -eq 7 ]]
 then 
-   echo "SEVEN"
+    echo "SEVEN"
 elif [[ $number -eq 8 ]]
 then 
-   echo "EIGHT"
+    echo "EIGHT"
 else
-   echo "NINE"
+    echo "NINE"
 fi
