59c8573fe203535b28885f7925b404f131fbcf7f
Revati6969 <61040382+Revati6969@users.noreply.github.com>
2020-02-24 14:50:52 +0530

    MODOFIED:Removed spaces and unnecessary lines

--- elseif/Unit.sh
+++ elseif/Unit.sh
@@ -2,19 +2,19 @@
 read -p "enter number" number
 if [[ $number -ge 10000 ]]
 then
-	echo "number must be less or equal to 10000"
+    echo "number must be less or equal to 10000"
 elif [[ $number -eq 1 ]]
 then 
-	echo "Unit"
+    echo "Unit"
 elif [[ $number -eq 10 ]]
 then
-	echo "Ten"
+    echo "Ten"
 elif [[ $number -eq 100 ]]
 then
-	echo "Hundred"
+    echo "Hundred"
 elif [[ $number -eq 1000 ]]
 then
-	echo "Thousand"
+    echo "Thousand"
 else
-	echo "Ten Thousand"
+    echo "Ten Thousand"
 fi
