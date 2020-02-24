40bf9c770c9984c846c48f09b61d35e7dcd9deb6
Revati6969 <61040382+Revati6969@users.noreply.github.com>
2020-02-24 14:52:52 +0530

    ADDED:Days of week

--- elseif/WeekDays.sh
+++ elseif/WeekDays.sh
@@ -2,30 +2,30 @@
 read -p "enter num:" day
 if [[ $day -lt 1 ]]
 then 
-	echo "number must be greater or equal to 1"
+     echo "number must be greater or equal to 1"
 elif [[ $day -gt 7 ]]
 then
-	echo "number must be lesser or equal to 7"
+     echo "number must be lesser or equal to 7"
 elif [[ $day -eq 1 ]]
 then
-	echo "SUNDAY"
+     echo "SUNDAY"
 elif [[ $day -eq 2 ]]
 then 
-	echo "MONDAY"
+     echo "MONDAY"
 elif [[ $day -eq 3 ]]
 then 
-   echo "TUESDAY"
+     echo "TUESDAY"
 elif [[ $day -eq 4 ]]
 then 
-   echo "WEDNESDAY"
+     echo "WEDNESDAY"
 elif [[ $day -eq 5 ]]
 then 
-   echo "THURSDAY"
+     echo "THURSDAY"
 elif [[ $day -eq 6 ]]
 then 
-   echo "FRIDAY"
+     echo "FRIDAY"
 else
-   echo "SATURDAY"
+     echo "SATURDAY"
 fi
 
 
