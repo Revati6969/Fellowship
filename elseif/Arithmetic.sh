0f23fcfcf30299d69ec5ee3ac7636722bfbd01e7
Revati6969 <61040382+Revati6969@users.noreply.github.com>
2020-02-24 14:48:08 +0530

    MODIFIED:Removed spaces

--- elseif/Arithmetic.sh
+++ elseif/Arithmetic.sh
@@ -10,28 +10,28 @@ result4=$(( $p*$q+$r ))
 #Minimum value
 if [[ $result1 -lt $result2 && $result1 -lt $c && $result1 -lt $result4 ]]
 then 
-	echo "$result1 is smaller"
+    echo "$result1 is smaller"
 elif [[ $result2 -lt $result1 && $result2 -lt $c && $result2 -lt $result4 ]]
 then
-	echo "$result2 is smaller"
+    echo "$result2 is smaller"
 elif [[ $result3 -lt $result1 && $result3 -lt $result2 && $result3 -lt $result4 ]]
 then
-	echo "$result3 is smaller"
+    echo "$result3 is smaller"
 elif [[ $result4 -lt $result1 && $result4 -lt $result2 && $result4 -lt $result3 ]]
 then
-	echo "$result4 is smaller"
+    echo "$result4 is smaller"
 fi
 #Maximum value
 if [[ $result1 -gt $result2 && $result1 -gt $result3 && $result1 -gt $result4 ]]
 then
-	echo "$result1 is greater"
+    echo "$result1 is greater"
 elif [[ $result2 -gt $result1 && $result2 -gt $result3 && $result2 -gt $result4 ]]
 then
-	echo "$result2 is greater"
+    echo "$result2 is greater"
 elif [[ $result3 -gt $result1 && $result3 -gt $result2 && $result3 -gt $result4 ]]
 then
-	echo "$result3 is greater"
+    echo "$result3 is greater"
 elif [[ $result4 -gt $result2 && $result4 -gt $result3 && $result4 -gt $result1 ]]
 then
-	echo "$result4 is greater"
+    echo "$result4 is greater"
 fi
