38d8a7b8451bf1cfdd4203889f167ff20015daf6
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-24 12:12:12 +0530

    ADDED: Find the magic number

---
+++ whileloop/MagicNumber.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+#find the magic number
+read -p "Assume the Number between 1 to 100: " number
+bigining=1
+end=100
+middle=$(( $bigining+$end/2 ))
+while [[ $begining -le $end ]]
+do
+	# find mindal number
+   if [[ $middle -eq $number ]]
+   then
+      echo $middle
+      break
+	elif [[ $number -lt $middle ]]
+   then
+      end=$middle
+      middle=$(( ($bigining+$end) / 2 ))
+   else
+      bigining=$middle
+      middle=$(( ($bigining+$end) / 2 ))
+   fi
+done
