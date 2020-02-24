fde957baf6a420ebf259e3ad736e556ec08d6a08
Revati6969 <61040382+Revati6969@users.noreply.github.com>
2020-02-24 15:17:46 +0530

    ADDED:Prime factors of number

--- forloop/PrimeFactor.sh
+++ forloop/PrimeFactor.sh
@@ -2,22 +2,22 @@
 read -p "enter num :": number
 for (( i=2; i<$number; i++ ))
 do
-	if [[ $(($number%$i)) -eq 0 ]]
-	then
-		for (( j=2; j<$i; j++ ))
-		do
-			if [[ $(($i%2)) -eq 0 ]]
-			then
-				flag=0
-			else
-				flag=1
-			fi
-		done
-	fi
-	if [[  $flag == 1 ]]
-	then
-		echo $i
-	fi
-	flag=0
+   if [[ $(($number%$i)) -eq 0 ]]
+   then
+        for (( j=2; j<$i; j++ ))
+	do
+	   if [[ $(($i%2)) -eq 0 ]]
+	   then
+		flag=0
+	   else
+		flag=1
+	   fi
+	done
+   fi
+   if [[  $flag == 1 ]]
+   then
+	echo $i
+   fi
+flag=0
 done
 
