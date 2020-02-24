b7a7947d25cc381900b5073c20894924ac24f360
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-24 14:17:50 +0530

    FIXED:Removed extra '$'

--- ifelse/MinMax.sh
+++ ifelse/MinMax.sh
@@ -23,20 +23,19 @@ then
 	echo "$number5  is smaller"
 fi
 
-if [[ $number1  -gt $b && $number1  -gt $c && $number1  -gt $number4 && $number1  -gt $$number5  ]]
+if [[ $number1  -gt $b && $number1  -gt $c && $number1  -gt $number4 && $number1  -gt $number5  ]]
 then
 	echo "$number1  is greater"
-elif [[ $number2 -gt $number1  && $number2 -gt $number3 && $number2 -gt $number4 && $number2 -gt $$number5  ]]
+elif [[ $number2 -gt $number1  && $number2 -gt $number3 && $number2 -gt $number4 && $number2 -gt $number5  ]]
 then
 	echo "$number2 is greater"
-elif [[ $number3 -gt $number1  && $number3 -gt $number2 && $number3 -gt $number4 && $number3 -gt $$number5  ]]
+elif [[ $number3 -gt $number1  && $number3 -gt $number2 && $number3 -gt $number4 && $number3 -gt $number5  ]]
 then
 	echo "$number3 is greater"
-elif [[ $number4 -gt $number2 && $number4 -gt $number3 && $number4 -gt $number1  && $number4 -gt $$number5  ]]
+elif [[ $number4 -gt $number2 && $number4 -gt $number3 && $number4 -gt $number1  && $number4 -gt $number5  ]]
 then
 	echo "$number4 is greater"
-elif [[ $$number5  -gt $number2 && $$number5  -gt $number3 && $$number5  -gt $number4 && $$number5  -gt $number1  ]]
+elif [[ $number5  -gt $number2 && $number5  -gt $number3 && $number5  -gt $number4 && $number5  -gt $number1  ]]
 then
 	echo "$number5 is greater"
-
 fi
