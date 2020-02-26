c224900968c906857c0083990738a6e2f4be8aa9
Revati Tekale <revitekale1910@gmail.com>
2020-02-26 12:31:27 +0530

    MODIFIED:Removed spaces

--- array/SumOfThreeIntegers.sh
+++ array/SumOfThreeIntegers.sh
@@ -1,25 +1,30 @@
 #!/bin/bash -x
+
+#Storing random values into array
 for (( index=0; index<=10; index++ ))
 do
-	temp=$(($RANDOM%20 + (-10)))
-	array[index]=$temp
-	temp=0
+   temp=$(($RANDOM%20 + (-10)))
+   array[index]=$temp
+   temp=0
 done
+
 flag=0
 size=${#array[@]}
+
+#Sum of three integers
 for(( index1=0 ; index1 < $size-2 ; index1++ ))
 do
-    for(( index=$index1+1; index2 < $size-1 ; index2++ ))
-    do
-        for(( index3=$index2+1; index3< $size ; index3++ ))
-        do
-            if(( "${array[index1]}+${array[index2]}+${array[index3]} ==0 " ))
-            then
-                echo "Triplet is : ${array[index1]}  ${array[index2]}  ${array[index3]} "
-                flag=1
-            fi
-        done
-    done
+   for(( index=$index1+1; index2 < $size-1 ; index2++ ))
+   do
+      for(( index3=$index2+1; index3< $size ; index3++ ))
+      do
+         if(( "${array[index1]}+${array[index2]}+${array[index3]} ==0 " ))
+         then
+             echo "Triplet is : ${array[index1]}  ${array[index2]}  ${array[index3]} "
+             flag=1
+         fi
+      done
+   done
 done
 
 if (( $flag == 0 ))
