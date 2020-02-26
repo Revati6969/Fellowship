43cf75f9d00f8868fa025f774fa3c0b7c5d25df5
Revati Tekale <revitekale1910@gmail.com>
2020-02-26 12:40:47 +0530

    MODIFIED:Removed spaces

--- array/SearchElement.sh
+++ array/SearchElement.sh
@@ -6,11 +6,11 @@ i=0
 #Storing random numbers into array
 while [[ $i -lt 10 ]]
 do
-	temp=$(($RANDOM%900 + 100))
-	echo $temp
-	Number[i]=$temp
-	temp=0
-	i=$(($i+1))
+   temp=$(($RANDOM%900 + 100))
+   echo $temp
+   Number[i]=$temp
+   temp=0
+   i=$(($i+1))
 done
 
 #Second smalles
@@ -22,11 +22,11 @@ small=${arr[0]}
 secsmall=${arr[0]}
 for (( i=0; i<$size; i++ ))
 do
-	if [[ $small -gt ${arr[i]} ]]
-	then
-		secsmall=$small
-		small=${arr[i]}
-	fi
+   if [[ $small -gt ${arr[i]} ]]
+   then
+	secsmall=$small
+	small=${arr[i]}
+   fi
 done
 echo $small "Smallest and"
 echo $secsmall "Second Smallest"
@@ -43,11 +43,11 @@ for (( i=0; i<$size; i++ ))
 do
    if [[ $large -lt ${arr1[i]} ]]
    then
-      seclarge=$large
-      large=$((${arr1[i]}))
-	elif [[ arr[i] -gt $seclarge ]]
-	then
-		seclarge=${arr[i]}
+       seclarge=$large
+       large=$((${arr1[i]}))
+   elif [[ arr[i] -gt $seclarge ]]
+   then
+       seclarge=${arr[i]}
    fi
 done
 echo $large "Largest and"
