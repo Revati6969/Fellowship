e3eb15a9e66ba6f4d96af805a316068ce00dca0d
Revati Tekale <revitekale1910@gmail.com>
2020-02-26 12:48:39 +0530

    REFACTORED:removed spaces

--- forloop/Harmonic.sh
+++ forloop/Harmonic.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -x
+
 read -p "enter num :": number
+
+#variable
 harmonic=0
+
 for (( index=1; index<=$number; index++ ))
 do 
-	harmonic=`echo  "$harmonic + (1/$index)" | bc -l`
+    harmonic=`echo  "$harmonic + (1/$index)" | bc -l`
 done
-	echo $harmonic
+	
+echo $harmonic
 
 
 
