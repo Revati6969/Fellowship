691421f31bf090d0c7772093bbc8a975c4605f62
Revati Tekale <revitekale1910@gmail.com>
2020-02-26 12:20:08 +0530

    REFACTORED:Removed spaces

--- array/RepeatedNumber.sh
+++ array/RepeatedNumber.sh
@@ -1,14 +1,16 @@
 #!bin/bash -x
+
 #variables
 count=0
+
 declare -a array
 for (( index=0; index<=100; index++ ))
 do
-	if [[ $index%11 -eq 0 ]]
-	then
-		array[count]=$index
-		echo ${array[count]}
-		((count++))
-	fi
+   if [[ $index%11 -eq 0 ]]
+   then
+	array[count]=$index
+	echo ${array[count]}
+	((count++))
+   fi
 done
 
