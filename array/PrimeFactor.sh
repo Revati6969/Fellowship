19e8d8779e28cf81629ee8f73f06548fb8da91f4
Revati Tekale <revitekale1910@gmail.com>
2020-02-26 12:19:09 +0530

    REFACTORED:Removed spaces

--- array/PrimeFactor.sh
+++ array/PrimeFactor.sh
@@ -3,16 +3,17 @@ counter=0
 
 while(($number%2==0))
 do
-	Factor[((counter++))]=2
-	number=$(($number/2))
+   Factor[((counter++))]=2
+   number=$(($number/2))
 done
+
 for(( index=3; index<=$number; index=$(($index+2)) ))
 do
-	while(($number%$index==0))
-	do
-		Factor[((counter++))]=$index
-		number=$(($number/$index))
-	done
+   while(($number%$index==0))
+   do
+      Factor[((counter++))]=$index
+      number=$(($number/$index))
+   done
 done
 
 echo "prime factors of $temp are: " ${Factor[@]}
